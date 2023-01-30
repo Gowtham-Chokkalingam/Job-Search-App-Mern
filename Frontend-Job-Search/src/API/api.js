0617943f1269269b92e792ac7282abf33f91d100
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2023-01-30 17:56:23 +0530

    Read me file

--- Frontend-Job-Search/src/API/api.js
+++ Frontend-Job-Search/src/API/api.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8080";
+// export const API_URL = "http://localhost:8080";
 
-// export const API_URL = "https://wild-erin-python-suit.cyclic.app";
+export const API_URL = "https://job-searchapp-server.onrender.com";
